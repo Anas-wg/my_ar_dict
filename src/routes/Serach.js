2483c023f6e5b225eadb68578e24ba8b93410e2e
CHO WangGi <70210457+Anas-wg@users.noreply.github.com>
2023-05-16 20:11:54 +0300

    feat : 검색 기능 고도화 일부 단어 및 철자를 통한 검색 기능 추가 한-아 검색, 아-한 검색 기능 추가

--- src/routes/Serach.js
+++ src/routes/Serach.js
@@ -2,34 +2,52 @@ import { CoreCompoent } from "../core/coreComponent";
 import { DATA } from "../worddata";
 
 export default class Search extends CoreCompoent {
-  render(){
+  render() {
     this.el.classList.add('search')
     this.el.innerHTML = /* html */`
       <input
         placeholder ="Enter the word mean"
       >
-      <button>
-        Search!
+      <button id="lang">
+        KR
       </button>
       <div>
         <ul>
-          <li class="result">값이 없다면 변하지 않음</li>
+          <li class="result">단어 등장 예정!</li>
         </ul>
       </div>
 
     `
     const inputEl = this.el.querySelector('input')
-    const btnEl = this.el.querySelector('button')
     const resultEl = this.el.querySelector('.result')
-    btnEl.addEventListener("click",()=> {
-      DATA
-        .filter(data => data.mean === inputEl.value)
-        .map(function(element){
-          resultEl.innerHTML = /* html */`
-          ${element.single} : ${element.mean}
-          `
-        })
+    const langSelectBtnEl = this.el.querySelector('#lang')
 
+    // 언어선택 버튼 - KR을 기본값으로 두고, class 에 "AR"이 추가되면 버튼의 내용이 AR 로 바뀌도록함
+    langSelectBtnEl.addEventListener('click', () => {
+      if (langSelectBtnEl.classList)
+      // toggle 을 사용하여 자유로운 전환이 가능하도록 함
+        langSelectBtnEl.classList.toggle('AR')
+      langSelectBtnEl.innerHTML = (langSelectBtnEl.classList == "AR") ? `AR` : `KR`
+    })
+
+    inputEl.addEventListener('input', () => {
+      DATA.map(item => {
+        // 만약 class에 "AR"이 활성화된 상태라면
+        if (langSelectBtnEl.classList == "AR") {
+          // 이때는 input value 와 일치하는 아랍어 단어를 찾도록 함
+          // 단수와 복수, 현재와 미완료형과 일치하는 모든 단어를 찾을 수 있도록 OR 연산자를 사용함
+          if (item.single.indexOf(inputEl.value) !== -1 || item.plural.indexOf(inputEl.value) !== -1) {
+            resultEl.innerHTML = `
+            ${item.single} - ${item.plural} : ${item.mean}
+            `}
+        }
+        // AR 이 없는 경우 = KR
+        if (item.mean.indexOf(inputEl.value) !== -1) {
+          resultEl.innerHTML = `
+            ${item.single} - ${item.plural} : ${item.mean}
+            `
+        }
+      })
     })
   }
 }
